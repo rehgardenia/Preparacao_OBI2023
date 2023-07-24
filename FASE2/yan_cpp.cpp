2d4b97d5aa3aedf19f0a18eb44330797cdc5b4b2
Renata Gardenia <121052380+rehgardenia@users.noreply.github.com>
2023-07-24 12:12:34 -0300

    p2_subcadeias- outro forma de realizar o codigo
    Nessa a complexidade é de O(n³)

---
+++ FASE2/yan_cpp.cpp
@@ -0,0 +1,39 @@
+// OBI2022 - Fase 2
+// Tarefa Subcadeias
+// Yan Silva
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+
+string s;
+
+int main()
+{
+    cin >> n >> s;
+
+    int resposta = 0;
+
+    for(int l = 0 ; l < n ; l++)
+    {
+        for(int r = l ; r < n ; r++)
+        {
+            bool ehPalindromo = true;
+
+            for(int pl = l, pr = r ; pl <= pr ; pl++, pr--){
+                if( s[pl] != s[pr] )
+                    ehPalindromo = false;
+                cout<< s.substr(pl,pr)<< endl ;
+            }
+
+
+            if( ehPalindromo )
+                resposta = max(resposta, r - l + 1);
+        }
+    }
+
+    cout << resposta << endl;
+}
