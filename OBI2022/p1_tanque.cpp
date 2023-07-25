73dc82dec167c0288a5b7eb21179a57f2df97f70
Renata Gardenia <121052380+rehgardenia@users.noreply.github.com>
2023-07-25 11:42:02 -0300

    Nivel 1

--- OBI2022/p1_tanque.cpp
+++ OBI2022/p1_tanque.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+
 using namespace std;
+
 double  consumo = 0.0, distancia = 0.0, inicio = 0.0;
 double resp = 0.0;
+
 int main ()
 {
   cin >> consumo >> distancia >> inicio;
   resp = (distancia/consumo) - inicio;
+  
   std::cout.precision(1);
   std::cout << std::fixed << resp << endl;
+  
   return 0;
 }
